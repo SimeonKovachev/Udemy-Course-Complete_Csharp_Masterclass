0631bc817a53d5526bd46661648f53380bc1b906
Simeon Kovachev <skcodingacc@gmail.com>
2023-10-21 21:07:26 +0300

    Learned so far from the course and resources i've got

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Console.WriteLine("Thats very strange typing in c# again");
+
+Console.Beep();
+
+Console.WriteLine("I beeped now type");
+
+string n1 = Console.ReadLine();
+
+Console.WriteLine(n1);*/
+
+//Second video 
+
+/*int number1;
+
+number1 = 2;
+
+int number2 = 30;
+
+int sum = number1 + number2;
+Console.WriteLine(sum);*/
+
+//Concat strings
+
+string myname = "Simo";
+
+string message = "My name is " + myname;
+
+string messageLower = message.ToLower();
+
+Console.WriteLine(messageLower);
